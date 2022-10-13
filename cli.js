b731adff75c173746770e101d5dfee322714db3d
Katie Cornette <kathcorn@email.unc.edu>
2022-10-13 13:31:05 -0400

    Trial 4.

--- cli.js
+++ cli.js
@@ -25,7 +25,7 @@ var latitude = args.n || (args.s * -1);
 var longitude = args.e || (args.w * -1);
 var day = args.d ? args.d : 1; 
   
-const URL = 'https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&temperature_unit=fahrenheit&timezone=' + timezone;
+const URL = 'https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&current_weather=true&temperature_unit=fahrenheit&timezone=' + timezone;
 
 const response = await fetch(URL); 
 
@@ -44,7 +44,6 @@ else{
         console.log("You will need your galoshes ");
 } 
 // -d command
-const day = args.d;  
 if (day == 0) {
   console.log("today.")
 } else if (day > 1) {
